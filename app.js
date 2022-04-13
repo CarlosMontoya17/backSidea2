d27453e9356924ea5e54b4f372bcaccb31fa8ccc
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-04-13 11:20:07 -0500

    Headers

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ const options = {
   
 https.createServer(options, app).listen(3030);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
+  
 /*
 app.listen(3030, () =>{
     console.log("Server is running");
