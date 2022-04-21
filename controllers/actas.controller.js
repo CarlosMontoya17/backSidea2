f4250b1bf63c87d00b92c610b38d452f10b99ada
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-04-21 14:04:25 -0500

    Nacimiento & Defuncion

--- controllers/actas.controller.js
+++ controllers/actas.controller.js
@@ -8,24 +8,62 @@ const options = {};
 
 exports.upPDF = (req, res) => {
     const file = req.file;
-    if(!file){
-        res.status(500).json({message: 'Please upload a file'});
+    if (!file) {
+        res.status(500).json({ message: 'Please upload a file' });
     }
-    else{
-        pdfExtract.extract(path.resolve('assets/docs/'+file.originalname), options).then(data =>{
+    else {
+        pdfExtract.extract(path.resolve('assets/docs/' + file.originalname), options).then(data => {
             const page = data.pages[0].content;
-            const tipe = page[13].str
-            if(tipe.includes("Nacimiento")){
-                res.send(`Es un ${tipe}`);
+            const tipo = page[13].str;
+            let curp, estado, nombre, apellidos;
+
+            if (tipo.includes("Acta")) {
+
+                switch (tipo) {
+                    case "Acta de Nacimiento":
+                        curp = page[2].str;
+                        estado = page[10].str;
+                        nombre = page[21].str;
+                        apellidos = page[23].str + " " + page[25].str;
+                        console.log(`tipo: ${tipo}`);
+                        console.log(`curp: ${curp}`);
+                        console.log(`estado: ${estado}`);
+                        console.log(`nombre: ${nombre}`);
+                        console.log(`apellidos: ${apellidos}`);
+                        data = { tipo, curp, estado, nombre, apellidos}
+
+                        res.send(data);
+                        break;
+                    case "Acta de Defunción":
+                        curp = page[4].str;
+                        estado = page[8].str;
+                        nombre = page[31].str;
+                        apellidos = page[32].str+" "+page[34].str;
+                        console.log(`tipo: ${tipo}`);
+                        console.log(`curp: ${curp}`);
+                        console.log(`estado: ${estado}`);
+                        console.log(`nombre: ${nombre}`);
+                        console.log(`apellidos: ${apellidos}`);
+                        data = { tipo, curp, estado, nombre, apellidos}
+                        res.send(data);
+                        break;
+                    default:
+                        res.status(500).json({message: "..."})
+                        break
+                }
+
+                
+
+
             }
-            else{
-                res.status(404).json({message: 'File doesnt exist'})
+            else {
+                res.status(404).json({ message: 'File doesnt exist' })
             }
-            
+
 
 
         }).catch(err => {
-            res.status(500).json({err})
+            res.status(500).json({ err })
         });
     }
 }
