2487fb18bb764db04b12fcaae5b8440db4d80c92
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-05-20 12:55:31 -0500

    Obtain Actas Request

--- controllers/actas_req.controller.js
+++ controllers/actas_req.controller.js
@@ -39,3 +39,17 @@ exports.commentsUp = async (req, res) => {
         res.status(500).json({message: 'Internal Error!'});
     });
 }
+
+
+exports.obtainAllRequets = async (req, res) => {
+    const id = req.usuarioID;
+    await actas_req.findAll({
+        where: { id_req: id }
+    }).then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.status(500).json({
+            message: 'Internal Error!'
+        })
+    });
+}
--- routes/actas_req.routes.js
+++ routes/actas_req.routes.js
@@ -5,5 +5,5 @@ module.exports = function(app) {
     app.post("/api/actas/requests/createOne/", verifyAuth, controller.createARequest);
     app.get("/api/actas/requests/getOneTask/", controller.getRequestNoAttended);
     app.put("/api/actas/requests/comment/:id", controller.commentsUp);
-
+    app.get("/api/actas/requests/obtainAll/", verifyAuth, controller.obtainAllRequets);
 }
