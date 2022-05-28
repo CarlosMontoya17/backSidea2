e7517ab58bce4f6ef1acc3e96746edfec6ebe486
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-05-28 10:07:43 -0500

    Preferences actas

--- controllers/actas_req.controller.js
+++ controllers/actas_req.controller.js
@@ -81,7 +81,7 @@ exports.upPDF = async (req, res) => {
 exports.whomRequested = async (req, res) => {
     const { id } = req.params;
 
-    await actas_req.findOne({where: {id}, attributes: ['id_req']}).then(data => {
+    await actas_req.findOne({where: {id}, attributes: ['id_req', 'preferences']}).then(data => {
         res.status(200).json(data);
     }).catch(err => {
         res.status(500).json({ message: 'Internal Error!' });
