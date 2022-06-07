60ce5ffcd02357e52eeceb9ae206fd1b6e60f7ed
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-06-07 02:42:26 -0500

    Rfc Req

--- controllers/rfc_req.controller.js
+++ controllers/rfc_req.controller.js
@@ -72,7 +72,7 @@ exports.upPdf = async (req, res) => {
 exports.getMyData = async (req, res) => {
     const { id } = req.params;
 
-    await rfc_req.findOne({where: {id}}).then(data => {
+    await rfc_req.findOne({where: {id}, attributes: ['id_req']}).then(data => {
         res.status(200).json(data);
     }).catch(err => {
         res.status(500).json({message: 'Internal Error!'});
