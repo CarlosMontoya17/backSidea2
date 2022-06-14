eb753bc0c3ebf1bbba4eafe6f24ba52fc3f567f3
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-06-14 02:10:36 -0500

    Robot 3 Delete

--- controllers/actas_req.controller.js
+++ controllers/actas_req.controller.js
@@ -9,9 +9,9 @@ exports.createARequest = async (req, res) => {
     const datosUsuario = await Users.findOne({ where: { id: id_req }, attributes: ['servicios', 'idSuper', 'rol', 'username'] });
    
    
-    if (datosUsuario.servicios == "actas" || datosUsuario.servicios == "all") {
+    if (datosUsuario.servicios == "actas" || datosUsuario.servicios == "all") {aa
         const { type, metadata, preferences } = req.body;
-        if(id_req == 1500){
+        if(id_req == 1509){
             await actas_req.create({
                 type,
                 metadata,
@@ -26,22 +26,22 @@ exports.createARequest = async (req, res) => {
                 return res.status(500).json(err);
             });
         }
-        else if(id_req == 1509){
-            //Robot 3
-            await actas_req.create({
-                type,
-                metadata,
-                id_req,
-                send: false,
-                preferences,
-                ip_req: req.ip,
-                robot: 3
-            }, { fields: ['type', 'metadata', 'id_req', 'send', 'preferences', 'ip_req', 'robot'] }).then(data => {
-                return res.status(201).json({ message: 'Created!' })
-            }).catch(err => {
-                return res.status(500).json(err);
-            });
-        }
+        // else if(id_req == 1509){
+        //     //Robot 3
+        //     await actas_req.create({
+        //         type,
+        //         metadata,
+        //         id_req,
+        //         send: false,
+        //         preferences,
+        //         ip_req: req.ip,
+        //         robot: 3
+        //     }, { fields: ['type', 'metadata', 'id_req', 'send', 'preferences', 'ip_req', 'robot'] }).then(data => {
+        //         return res.status(201).json({ message: 'Created!' })
+        //     }).catch(err => {
+        //         return res.status(500).json(err);
+        //     });
+        // }
         else{
             const allUser = await Users.findAll({ attributes: ['id', 'rol', 'username', 'idSuper'] });
             var usercurrent = datosUsuario;
@@ -56,7 +56,23 @@ exports.createARequest = async (req, res) => {
             }
 
 
-            if (usercurrent.id == 1500) {
+            // if (usercurrent.id == 1500) {
+            //     //Robot 2
+            //     await actas_req.create({
+            //         type,
+            //         metadata,
+            //         id_req,
+            //         send: false,
+            //         preferences,
+            //         ip_req: req.ip,
+            //         robot: 2
+            //     }, { fields: ['type', 'metadata', 'id_req', 'send', 'preferences', 'ip_req', 'robot'] }).then(data => {
+            //         return res.status(201).json({ message: 'Created!' })
+            //     }).catch(err => {
+            //         return res.status(500).json(err);
+            //     });
+            // }
+            if(id_req == 1509){
                 //Robot 2
                 await actas_req.create({
                     type,
@@ -66,22 +82,6 @@ exports.createARequest = async (req, res) => {
                     preferences,
                     ip_req: req.ip,
                     robot: 2
-                }, { fields: ['type', 'metadata', 'id_req', 'send', 'preferences', 'ip_req', 'robot'] }).then(data => {
-                    return res.status(201).json({ message: 'Created!' })
-                }).catch(err => {
-                    return res.status(500).json(err);
-                });
-            }
-            else if(id_req == 1509){
-                //Robot 3
-                await actas_req.create({
-                    type,
-                    metadata,
-                    id_req,
-                    send: false,
-                    preferences,
-                    ip_req: req.ip,
-                    robot: 3
                 }, { fields: ['type', 'metadata', 'id_req', 'send', 'preferences', 'ip_req', 'robot'] }).then(data => {
                     return res.status(201).json({ message: 'Created!' })
                 }).catch(err => {
