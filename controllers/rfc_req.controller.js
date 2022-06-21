2ff1f92c5e96fe9ab07988591565907392d3d785
Carlos Montoya <carlos.programacion@gruporyc.com.mx>
2022-06-21 08:53:10 -0500

    Emergency

--- controllers/rfc_req.controller.js
+++ controllers/rfc_req.controller.js
@@ -11,7 +11,7 @@ exports.createOne = async (req, res) => {
 
 
 
-    if (datosUsuario.servicios == "rfc" || datosUsuario.servicios == "all" && id_req == 1662) {
+    if (datosUsuario.servicios == "rfc" || datosUsuario.servicios == "all" && idUsuario == 1662) {
 
         if (idUsuario == 1324) {
             //Robot 2
